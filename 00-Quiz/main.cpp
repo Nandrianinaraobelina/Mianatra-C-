d6e630243133c27582fcadc8fbecb2b29431c5b8
nandrianina <herynandrianina575@mail.com>
2025-10-25 16:34:08 +0300

    MAJ

--- 00-Quiz/main.cpp
+++ 00-Quiz/main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main()
 {
-    //DANS OPENCLASSROOM l'exercice QUIZ: https://openclassrooms.com/fr/courses/1894236-apprenez-a-programmer-en-c%2B%2B/exercises/4399
+    //AO AMIN'NY OPENCLASSROOM ILAY exercice QUIZ,IO NY LIEN : https://openclassrooms.com/fr/courses/1894236-apprenez-a-programmer-en-c%2B%2B/exercises/4399
 
 }
